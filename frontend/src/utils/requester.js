6ec27e1d38e886d11bb6e1ac2dfeeec93e987de6
BoykoPetevBoev <boykopetevboev@gmail.com>
2020-12-09 15:30:15 +0200

    update frontend

--- frontend/src/utils/requester.js
+++ frontend/src/utils/requester.js
@@ -1,5 +1,35 @@
-
+const axios = require('axios');
 const url = "http://localhost/44310"
+
+
+
+
+async function httpRequest(method, body, url) {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    // xhr.onreadystatechange = function () { 
+    //     if (xhr.readyState === 4 && xhr.status === 200) { 
+    //         result.innerHTML = this.responseText; 
+    //     } 
+    // }; 
+
+    const data = JSON.stringify(body);
+    xhr.send(data);
+}
+
+async function axiosRequest(method, body, url) {
+    const response = await axios({
+        method: method,
+        url: url,
+        headers: { 'X-Requested-With': 'XMLHttpRequest' },
+        responseType: 'json',
+        data: JSON.stringify(body)
+    });
+    console.log(response);
+}
 
 async function fetchRequest(method, body, url) {
     const promise = await fetch(url, {
@@ -11,12 +41,12 @@ async function fetchRequest(method, body, url) {
 }
 
 async function userLogin(body) {
-    const promise = await fetchRequest('POST', body, `${url}/login`);
+    const promise = await axiosRequest('POST', body, `${url}/login`);
     return await handleUserRequest(promise);
 }
 
 async function userRegister(body) {
-    const promise = await fetchRequest('POST', body, `${url}/register`);
+    const promise = await httpRequest('POST', body, `${url}/register`);
     return await handleUserRequest(promise);
 }
 
