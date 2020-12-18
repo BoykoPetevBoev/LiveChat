c6be57125ce3b914b523e57f065edbe685b59e62
BoykoPetevBoev <boykopetevboev@gmail.com>
2020-12-18 16:58:28 +0200

    update: Frontend

--- frontend/src/utils/requester.js
+++ frontend/src/utils/requester.js
@@ -4,32 +4,32 @@ const url = "http://localhost/44310"
 
 
 
-async function httpRequest(method, body, url) {
+// async function httpRequest(method, body, url) {
 
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
+//     const xhr = new XMLHttpRequest();
+//     xhr.open(method, url, true);
+//     xhr.setRequestHeader("Content-Type", "application/json");
 
-    // xhr.onreadystatechange = function () { 
-    //     if (xhr.readyState === 4 && xhr.status === 200) { 
-    //         result.innerHTML = this.responseText; 
-    //     } 
-    // }; 
+//     // xhr.onreadystatechange = function () { 
+//     //     if (xhr.readyState === 4 && xhr.status === 200) { 
+//     //         result.innerHTML = this.responseText; 
+//     //     } 
+//     // }; 
 
-    const data = JSON.stringify(body);
-    xhr.send(data);
-}
+//     const data = JSON.stringify(body);
+//     xhr.send(data);
+// }
 
-async function axiosRequest(method, body, url) {
-    const response = await axios({
-        method: method,
-        url: url,
-        headers: { 'X-Requested-With': 'XMLHttpRequest' },
-        responseType: 'json',
-        data: JSON.stringify(body)
-    });
-    console.log(response);
-}
+// async function axiosRequest(method, body, url) {
+//     const response = await axios({
+//         method: method,
+//         url: url,
+//         headers: { 'X-Requested-With': 'XMLHttpRequest' },
+//         responseType: 'json',
+//         data: JSON.stringify(body)
+//     });
+//     console.log(response);
+// }
 
 async function fetchRequest(method, body, url) {
     const promise = await fetch(url, {
@@ -47,7 +47,7 @@ async function userLogin(body) {
 }
 
 async function userRegister(body) {
-    const promise = await httpRequest('POST', body, `${url}/register`);
+    const promise = await fetchRequest('POST', body, `${url}/register`);
     return await handleUserRequest(promise);
 }
 
