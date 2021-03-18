c9626b731f3e8414b57e845315b6ff08c847fbdc
BoykoPetevBoev <boykopetevboev@gmail.com>
2021-03-18 15:21:02 +0200

    feat: room model

--- backend-js/database/models/Room.js
+++ backend-js/database/models/Room.js
@@ -2,5 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RoomSchema = new Schema({
-    
+    name: {
+        type: String
+    },
+    messages: [],
+    members: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
 })
+
+module.exports = mongoose.model('Room', RoomSchema);
