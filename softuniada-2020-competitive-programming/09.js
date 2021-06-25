b430a6583625e16f6f7ab419f41d86df37faeaf8
BoykoPetevBoev <boykopetevboev@gmail.com>
2021-06-25 18:39:50 +0300

    update

--- softuniada-2020-competitive-programming/09.js
+++ softuniada-2020-competitive-programming/09.js
@@ -1,22 +1,23 @@
 function solve(input) {
     let target = Number(input.shift());
-    const validCoins = [1, 10, 25, 100, 1000, 2500, 10000, 100000, 250000, 1000000, 10000000, 25000000 ];
     let usedCoins = 0;
-    let coinIndex = validCoins.length - 1;
-    
-    while(target > 0 && coinIndex >= 0) {
-        const currentCoin = validCoins[coinIndex]
+    const result = []
 
-        if(currentCoin > target) {
-            coinIndex--;
-            continue;
-        }
+    for (let i = 0; i < 100; i++) {
+        result[i] = i;
+    }
+    for (let i = 10; i < 100; i++) {
+        result[i] = Math.min(result[i - 10] + 1, result[i]);
+    }
+    for (let i = 25; i < 100; i++) {
+        result[i] = Math.min(result[i - 25] + 1, result[i]);
+    }
 
-        target -= currentCoin;
-        usedCoins++;
+    while (target > 0) {
+        usedCoins += result[target % 100];
+        target = Math.floor(target / 100);
     }
     console.log(usedCoins);
-
 }
 
 solve([39]);
